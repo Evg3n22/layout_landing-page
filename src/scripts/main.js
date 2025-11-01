a498e33b969a6b7ad6b13e951d2bb2c669a41850
Evg3n22 <evgen.gulevaty@gmail.com>
2025-11-01 10:26:29 +0200

    fix fonts, hover phones and form reload problerm

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const inputs = document.querySelectorAll('.contact__form-textbox');
+const form = document.getElementById('contact__form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  inputs.forEach((element) => {
+    element.value = '';
+  });
+});
+
+const topBar = document.querySelector('.top-bar');
+const phoneIcon = document.querySelector('.icon--phone');
+const phoneInfo = document.querySelector('.phones-info');
+
+phoneIcon.addEventListener('mouseenter', () => {
+  phoneInfo.style.display = 'block';
+  phoneInfo.style.position = 'absolute';
+  phoneInfo.style.right = '30%';
+  phoneInfo.style.top = '10px';
+});
+
+topBar.addEventListener('mouseleave', () => {
+  phoneInfo.style.display = 'none';
+});
